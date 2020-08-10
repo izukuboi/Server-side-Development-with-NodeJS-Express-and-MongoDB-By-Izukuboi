e39d40fa9c13180af18d692e698eff530aba750b
izukuboi <36083635+izukuboi@users.noreply.github.com>
2020-08-10 17:40:45 -0400

    Passport

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session'); 
 var FileStore = require('session-file-store')(session);
+var passport = require('passport');
+var authenticate = require('./authenticate');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -43,14 +46,16 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 
 function auth(req, res, next){
-  console.log(req.session);
 
-  if(!req.session.user){
+  if(!req.user){
 
     var err = new Error('You are not authenticated');
  
@@ -58,19 +63,8 @@ function auth(req, res, next){
     return next(err);
   }
   else {
-    if (req.session.user === 'authenticated'){
-      next();
-    }
-    else {
-      var err = new Error('You are no authenticated');
-  
-
-      err.status = 401;
-      return next(err);
-    }
+    next();
   }
-
-
 }
 
 app.use(auth);
