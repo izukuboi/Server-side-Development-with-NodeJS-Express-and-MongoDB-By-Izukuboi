43f77d14a3a2a90503239d5a567c311f41e09327
izukuboi <36083635+izukuboi@users.noreply.github.com>
2020-08-29 21:11:59 -0400

    Passport Facebook

--- authenticate.js
+++ authenticate.js
@@ -4,6 +4,7 @@ var User = require('./models/user');
 var JwtStrategy =  require('passport-jwt').Strategy;
 var ExtractJwt = require('passport-jwt').ExtractJwt;
 var jwt = require('jsonwebtoken');
+var FacebookTokeStrategy = require('passport-facebook-token');
 
 var config = require('./config');
 
@@ -48,6 +49,39 @@ exports.verifyAdmin = (req, res, next) => {
         return next(err);
     }
 }
+
+exports.facebookPassport =  passport.use(new 
+FacebookTokeStrategy({
+    clientID: config.facebook.clientId,
+    clientSecret: config.facebook.clientSecret 
+    }, (accessToken, refreshToken, profile, done) => {
+        User.findOne({facebookId: profile.id}, (err, user)=> { // al autenticarse primero busca el usuario
+        
+            if (err){
+                return done(err, false);
+            }
+            if (!err && user !== null){
+                return done(null, user);
+            }
+            else {//Registro de usuario primer login
+                user =  new User({ username: 
+                profile.displayName});
+                user.facebookId = profile.id;//para que funcione el login
+                user.firstname = profile.name.givenName;
+                user.lastname = profile.name.familyName;
+                user.save((err, user) => {
+                    if (err){
+                        return done(err, false);
+                    }
+                    else{
+                        return done(null, user);
+                    }
+                })
+            }
+        });
+    }
+
+));
 
 //eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZjM2MGM4ZGIyNmFhNzBhYWM4MGY1YTEiLCJpYXQiOjE1OTczNzc3NTQsImV4cCI6MTU5NzM4MTM1NH0.VCSXm2ZaI5mDDCvPb3tSgDKemYNNdtayiB8A3_M_PGk
 //5f360defb26aa70aac80f5a3
--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-098760-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '326986611985347',
+        clientSecret: '53cc8faec3db9528af5ec12f4cbce19e'
+    }
 }
